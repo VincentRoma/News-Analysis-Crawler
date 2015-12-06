c7d2a86ef9f4f6923a7de307874cd2806f114592
VincentRoma <roma.vincent@gmail.com>
2015-12-06 03:28:31 +0100

    Fix css + statistiques

--- home/views.py
+++ home/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.db.models import Q
 from post.models import Post
+from news.models import News
 
 def HomeViews(request):
     slider_posts = Post.objects.filter(slider=True)
     posts = Post.objects.all()[:20]
-    return render(request, 'blog/home.html', {'posts': posts, 'slider_posts': slider_posts, 'state': 'home'})
+
+    ## Get Statistics ##
+    nb_news = News.objects.all().count()
+    nb_posts = Post.objects.all().count()
+    nb_refugee = News.objects.filter(title__contains="refugee").count()
+    nb_terror = News.objects.filter(Q(title__contains="ISIS") | Q(title__contains="Daesh") | Q(title__contains="terror")).count()
+
+    return render(request, 'blog/home.html', {
+        'posts': posts,
+        'slider_posts': slider_posts,
+        'state': 'home',
+        'nb_refugee': nb_refugee,
+        'nb_terror': nb_terror,
+        'nb_posts': nb_posts,
+        'nb_news': nb_news
+    })
